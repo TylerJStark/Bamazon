d7f703f51fe49aa5d4de5b7e2dbe9583b755b49a
Dova <Tyler4276@gmail.com>
2018-10-21 19:56:42 -0400

    Fixed some bugs I found

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,8 +24,9 @@ function listItems () {
       console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
     }
     console.log("-----------------------------------");
+    questions();
   });
-  questions();
+  
 }
 
 function questions() {
@@ -37,21 +38,35 @@ function questions() {
       name: "itemID"
     },
     {
-      type: "password",
+      type: "input",
       message: "How many units would you like to buy?",
       name: "unitsBuy"
     }
   ])
   .then(function(inquirerResponse) {
     var item = inquirerResponse.itemID - 1
+
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       var quantity = res[item].stock_quantity - inquirerResponse.unitsBuy;
-      if (res[i].stock_quantity > inquirerResponse.unitsBuy) {
-        // var sql = "UPDATE products SET stock_quantity = " quantity "WHERE item_id = " item;
-        console.log(item);
-        console.log(quantity);
-      } else if (res[i].stock_quantity < inquirerResponse.unitsBuy) {
+      var product = res[item].product_name
+
+      if (res[item].stock_quantity >= inquirerResponse.unitsBuy) {
+        var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: quantity
+          },
+          {
+            item_id: item
+          }
+        ],
+        function(err, res) {
+          console.log("Thank you for buying " + inquirerResponse.unitsBuy + " " + product + "!");
+          console.log(item);
+        }); 
+      } else if (res[item].stock_quantity < inquirerResponse.unitsBuy) {
         console.log("Sorry, we do not have the stock to fulfill the request.")
       } else {
         console.log("Oops! Something went wrong!");
